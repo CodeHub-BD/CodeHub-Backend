67ee80fe2e5360fd5b1cce6fd15c6fa0662d0e61
KhanShaheb34 <shakirul34@student.sust.edu>
2020-04-02 20:47:32 +0600

    Created user model

---
+++ models/userModel.js
@@ -0,0 +1,90 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have a name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'A user must have a name!'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'User must have a password!'],
+    select: false,
+  },
+  profilePic: {
+    type: String,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  contribution: {
+    type: Number,
+    default: 0,
+  },
+  social: [
+    {
+      name: {
+        type: String,
+        enum: ['facebook', 'twitter', 'instagram', 'github', 'stackoverflow'],
+      },
+      url: {
+        type: String,
+      },
+    },
+  ],
+  submissions: [
+    {
+      type: String,
+    },
+  ],
+  userStat: {
+    submission: {
+      type: Number,
+      default: 0,
+    },
+    ac: {
+      type: Number,
+      default: 0,
+    },
+    wa: {
+      type: Number,
+      default: 0,
+    },
+    tle: {
+      type: Number,
+      default: 0,
+    },
+    mle: {
+      type: Number,
+      default: 0,
+    },
+    re: {
+      type: Number,
+      default: 0,
+    },
+  },
+  institute: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+});
+
+const userModel = mongoose.model('User', userSchema);
+module.exports = userModel;
