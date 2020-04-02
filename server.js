fe56d64c70a291b04784502522c0622678400b47
Shakirul Hasan <shakirul34@student.sust.edu>
2020-04-02 10:19:04 +0600

    Environment config fixed

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: './config.env' });
+require('dotenv').config();
 const app = require('./app');
 const mongoose = require('mongoose');
 
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || 'localhost';
 
 //connect to DATABASE
-
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
